031ab10041f3194859ab43a79b7c5e7a07b92484
Ravi Soni <soniravi829@gmail.com>
2023-11-09 15:58:26 +0530

    loader

--- app/page.tsx
+++ app/page.tsx
@@ -262,27 +262,55 @@ export default function Home() {
                 handleFormSubmit(e);
               }}
             >
-              <TextArea
-                autoFocus
-                value={text}
-                onChange={(e) => {
-                  setText(e.target.value);
-                }}
-                onFocus={() => {
-                  if (state === "error") {
-                    setState("ready");
-                  }
-                }}
-                disabled={state === "playing" || state === "streaming"}
-                className="self-stretch"
-                variant="soft"
-                size="3"
-                maxLength={2000}
-                rows={5}
-                name="syn-text"
-                id="syn-text"
-                placeholder="Write here..."
-              />
+              <div className="relative self-stretch">
+                <TextArea
+                  autoFocus
+                  value={text}
+                  onChange={(e) => {
+                    setText(e.target.value);
+                  }}
+                  onFocus={() => {
+                    if (state === "error") {
+                      setState("ready");
+                    }
+                  }}
+                  disabled={state === "playing" || state === "streaming"}
+                  className="self-stretch relative"
+                  variant="soft"
+                  size="3"
+                  maxLength={2000}
+                  rows={5}
+                  name="syn-text"
+                  id="syn-text"
+                  placeholder="Write here..."
+                />
+                {state === "streaming" && (
+                  <div className="absolute bg-transparent bg-white  z-10 top-1/3 left-1/2 flex items-center justify-center">
+                    <div className="flex items-center">
+                      <svg
+                        className="animate-spin h-8 w-8 text-green-800"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          stroke-width="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                )}
+              </div>
               <Badge className="self-end tabular-nums" variant="surface">
                 {text.length} / {MAX_TEXT_LENGTH}
               </Badge>
@@ -303,7 +331,6 @@ export default function Home() {
                     </Flex>
                   </Button>
                 )}
-                {state === "streaming" && "Synthesizing..."}
                 {state === "streaming" && (
                   <Button type="button" onClick={stopAudio}>
                     <Flex gap={"2"} align={"center"}>
